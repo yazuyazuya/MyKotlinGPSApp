1cc1cc56a50d96d196089b6098862cb657f409d4
yazuyazuya <kzyono-1999@docomo.ne.jp>
2019-07-03 13:25:31 +0900

    Initial commit

---
+++ app/src/main/java/com/example/yazuyazuya/mykotlingpsapp/MainActivity.kt
@@ -0,0 +1,142 @@
+package com.example.yazuyazuya.mykotlingpsapp
+
+import android.Manifest
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.location.LocationManager
+import android.support.v4.app.ActivityCompat
+import android.content.pm.PackageManager
+import android.support.v4.content.ContextCompat
+import android.content.Context
+import android.content.Intent
+import android.location.Location
+import android.location.LocationListener
+import android.widget.TextView
+import android.location.LocationProvider
+import android.provider.Settings
+import android.widget.Toast
+import android.util.Log
+
+class MainActivity : AppCompatActivity(), LocationListener {
+
+    // lateinit: late initialize to avoid checking null
+    private lateinit var locationManager: LocationManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    1000)
+        } else {
+            locationStart()
+
+            if (::locationManager.isInitialized) {
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        1000,
+                        50f,
+                        this)
+            }
+
+        }
+    }
+
+    private fun locationStart() {
+        Log.d("debug", "locationStart()")
+
+        // Instances of LocationManager class must be obtained using Context.getSystemService(Class)
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Log.d("debug", "location manager Enabled")
+        } else {
+            // to prompt setting up GPS
+            val settingsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(settingsIntent)
+            Log.d("debug", "not gpsEnable, startActivity")
+        }
+
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1000)
+
+            Log.d("debug", "checkSelfPermission false")
+            return
+        }
+
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                1000,
+                50f,
+                this)
+
+    }
+
+    /**
+     * Android Quickstart:
+     * https://developers.google.com/sheets/api/quickstart/android
+     *
+     * Respond to requests for permissions at runtime for API 23 and above.
+     * @param requestCode The request code passed in
+     * requestPermissions(android.app.Activity, String, int, String[])
+     * @param permissions The requested permissions. Never null.
+     * @param grantResults The grant results for the corresponding permissions
+     * which is either PERMISSION_GRANTED or PERMISSION_DENIED. Never null.
+     */
+    override fun onRequestPermissionsResult(
+            requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == 1000) {
+            // 使用が許可された
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("debug", "checkSelfPermission true")
+
+                locationStart()
+
+            } else {
+                // それでも拒否された時の対応
+                val toast = Toast.makeText(this,
+                        "これ以上なにもできません", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+        }
+    }
+
+    override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+        when (status) {
+            LocationProvider.AVAILABLE ->
+                Log.d("debug", "LocationProvider.AVAILABLE")
+            LocationProvider.OUT_OF_SERVICE ->
+                Log.d("debug", "LocationProvider.OUT_OF_SERVICE")
+            LocationProvider.TEMPORARILY_UNAVAILABLE ->
+                Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE")
+        }
+    }
+
+    override fun onLocationChanged(location: Location) {
+        // Latitude
+        val textView1 = findViewById<TextView>(R.id.text_view1)
+        val str1 = "緯度:" + location.getLatitude()
+        textView1.text = str1
+
+        // Longitude
+        val textView2 = findViewById<TextView>(R.id.text_view2)
+        val str2 = "経度:" + location.getLongitude()
+        textView2.text = str2
+    }
+
+    override fun onProviderEnabled(provider: String) {
+
+    }
+
+    override fun onProviderDisabled(provider: String) {
+
+    }
+}
